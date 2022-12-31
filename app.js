b5c8bf3b6e409c22a11c4b4092b4e6851cd660d4
kmachalica <kmachalica98@gmail.com>
2022-12-31 18:51:17 +0100

    edited

--- app.js
+++ app.js
@@ -1,68 +1,66 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const shooterPoints = [-15,0,-20,20,-10,50,10,50,20,20,15,0]               //tutaj mam punkty armatki do łatwiejszego narysowania
-let isShootPossible = true;         //tutaj zapisuje się czy mogę oddać strzał (piłeczki nie są w ruchu)
-
-
 canvas.width = 400;
 canvas.height = 500;
 
+let isShootPossible = true;
+
+//BALL OBJ////BALL OBJ////BALL OBJ////BALL OBJ////BALL OBJ////BALL OBJ//
+
+
+
+
+//BOX OBJ////BOX OBJ////BOX OBJ////BOX OBJ////BOX OBJ////BOX OBJ////BOX OBJ//
 
 
 
 
 
 
+//FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS//
+const createBoard = ()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
 
-//FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS////FUNCTIONS//
-function createTable()          //create current state of game
-{
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.beginPath();
-    ctx.fillStyle = "#999";
-    ctx.fillRect(0,0,canvas.width,canvas.height);                   //tworze pole gry
-    ctx.closePath()
+    drawShooter()
 
-    //rysujemy armate
-    drawShooter('black')
-    
     canvas.addEventListener('mousemove',getAim)
-    canvas.addEventListener('click',()=>{isShootPossible=false})
-    
-
 }
 
-const getAim = (e)=>{ 
-    if(isShootPossible){
-        let x = e.clientX-window.innerWidth/2;       //lewa strona pokazuje mi X na minusie - łatwiej operować kierunkiem strzału
-        let y = e.clientY-30;
-        //na tej podstawie należy wyrysować celownik - linię przerywaną                       
-        canvas.addEventListener('click',()=>{
-            if(isShootPossible){
-                shootBalls(x,y)
-            }
-        })
-    }
-
-}
-
-const drawShooter = (color,ankle)=>{
+const drawShooter=()=>{
+    let shooterPoints = [185,500,180,480,190,450,210,450,220,480,215,500]
     ctx.beginPath();
-    ctx.translate(200,500)
-    ctx.rotate(180*Math.PI/180);
-    ctx.moveTo(0,0);
-    ctx.fillStyle = color;
+    ctx.fillStyle = "black"
+    ctx.moveTo(200,500);
     for(let i=0;i<shooterPoints.length;i+=2)
     {
         ctx.lineTo(shooterPoints[i],shooterPoints[i+1])
     }
     ctx.closePath()
     ctx.fill()
-    ctx.translate(200,500)
-    ctx.rotate(180*Math.PI/180)                         //przywracam poprzednie wartości
-    
-    
 }
-//START////START////START////START////START////START////START////START////START////START//
-createTable()
+
+const getAim = (e)=>{
+    
+    if(isShootPossible){
+        let x = e.clientX-window.innerWidth/2;
+        let y = e.clientY-30;
+        canvas.onclick = ()=>{shoot(x+200,y)}}
+}
+const shoot = (x,y)=>{
+    if(isShootPossible&&y+15<450){
+        isShootPossible=false
+        let [velX,velY] = velocity(x,y,200,450,1.25,1.75) 
+    }
+
+}
+
+const velocity = (x,y,beginX,beginY,maxDis,minDis)=>{               //x,y ->współrz.;beginX,beginY ->współrz. pocz.;min/maxDis -> maks dystans w jednym intervale
+        let [rX,rY] = [x-beginX,y-beginY];          //rzeczywista odległość
+        let velY = 1;
+        if(y==0){let velX = 0}
+        else{}
+
+}
+/////////////////////////////////////////////
+createBoard()
